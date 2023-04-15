a485a71da1b1af583b61d910f16dc39d01560679
shuvo <shuvoh38@gmail.com>
2023-04-15 17:06:36 +0600

    added common css

--- recipe_frontend/src/views/HomeView.vue
+++ recipe_frontend/src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="home-test">
+  <div>
     Home Page q
     <Layout >
       <h2>I am Layout</h2>
+      <button>Click ME</button>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat beatae reprehenderit hic quod accusantium, quae deserunt assumenda voluptate neque cupiditate pariatur! Quod eos ea modi eveniet porro enim animi ipsam harum hic ipsum quaerat quas, blanditiis amet dolores voluptas saepe recusandae aspernatur placeat unde optio commodi eum omnis iure aliquid? Vitae, vel libero? Voluptatibus reprehenderit, porro alias culpa amet veniam labore quod recusandae exercitationem sapiente odit illo enim commodi tempore maiores tenetur adipisci explicabo sunt dolorem ad nihil id repellendus quia. Neque quo omnis corrupti fuga reiciendis. Porro ipsa quae sequi blanditiis fugiat voluptate. Labore dicta quidem alias in blanditiis architecto libero explicabo omnis facilis, veniam accusamus nemo voluptas exercitationem eligendi reprehenderit! Libero perferendis quidem est. Hic soluta repudiandae illo rem ipsa facilis accusantium. Sed maxime, alias soluta reiciendis quam expedita ullam praesentium dicta sint, nobis inventore a accusantium est suscipit nostrum voluptatum enim nemo possimus veritatis quia. Fugit molestiae voluptatem exercitationem dolorum, porro numquam, illo omnis neque eveniet maxime ullam. Deleniti, obcaecati. Velit ipsam fugiat architecto incidunt illum laborum beatae aut consequatur adipisci aspernatur hic, quisquam, dolore autem. Minus, ab beatae! Dolorum, sunt saepe. Nam quisquam amet eos dolorem, perferendis dolores enim, voluptatem possimus nostrum sed itaque error aliquid praesentium fuga commodi natus dignissimos consectetur, sint quibusdam ex doloribus repudiandae alias? Aliquam, dicta aspernatur quaerat vitae repellendus tempore beatae iusto eius eaque deserunt optio tempora ab dolorem tenetur sed, dolorum architecto esse nam aperiam quod iste officia. Fugiat sapiente veritatis deserunt harum quidem! Sapiente numquam itaque reiciendis a pariatur. Excepturi ducimus suscipit dolor optio nesciunt praesentium nisi incidunt ipsa a, sunt rerum corrupti earum adipisci modi eligendi, officiis laborum. Placeat error similique autem atque, dolorum recusandae officiis molestiae a. Dolorum minus doloremque obcaecati. Dolorum neque libero cupiditate deleniti saepe quidem voluptatibus temporibus illum molestiae eligendi eius quam iure vero consequuntur modi, nihil officia, distinctio earum minus praesentium reiciendis vel quibusdam laborum? Commodi, ab doloribus harum quos nemo modi quidem dolores error reprehenderit sapiente iure vero ea temporibus soluta laudantium nam adipisci repellendus necessitatibus minima. Rerum ad id possimus laudantium molestiae tempore. Officiis blanditiis molestias illo, eveniet magni neque iste quas quo nobis, asperiores placeat? Obcaecati laborum officia numquam non incidunt ullam ut quidem magni natus iste sit ratione illo recusandae blanditiis accusamus debitis expedita ea aut, quos sint vero libero, quam impedit. Itaque nisi, sequi, minus maxime delectus voluptatibus, veniam consequatur suscipit quam facere reiciendis placeat architecto exercitationem nostrum minima ea perferendis? Sequi maiores, nulla ipsum mollitia quibusdam explicabo non deserunt animi maxime amet reiciendis distinctio blanditiis doloribus quas libero tempore illum incidunt ducimus. Cupiditate voluptates minima tempora est sunt excepturi voluptatibus sequi, explicabo fuga omnis facere ex distinctio, commodi et amet magnam eveniet veritatis iusto totam magni molestiae soluta impedit, non nam? Consequatur magnam provident, corporis ipsa fugit, fuga eaque error omnis perferendis ipsum nihil harum, sunt dolorum? Labore, odio laboriosam maiores architecto vero, veritatis consectetur, cupiditate repellat aspernatur sit et! Cum itaque fugit eligendi beatae eaque, amet similique, soluta quas ut aut vero animi repudiandae dicta rerum?</p>
     </Layout>
   </div>
 </template>
@@ -17,7 +19,6 @@ export default {
 </script>
 
 <style >
-.home-test{
-  color: aqua;
-}
+
+
 </style>
